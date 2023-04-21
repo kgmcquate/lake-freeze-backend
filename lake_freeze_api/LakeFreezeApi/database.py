bf1d2fa491fdbc3dbbb9c93289b0937855f1f099
EC2 Default User <ec2-user@ip-172-31-34-68.ec2.internal>
2023-04-21 22:45:45 +0000

    working on weather etl

--- lake_freeze_api/LakeFreezeApi/database.py
+++ lake_freeze_api/LakeFreezeApi/database.py
@@ -7,6 +7,8 @@ secret_arn = os.environ.get("DB_CREDS_SECRET_ARN", "arn:aws:secretsmanager:us-ea
 
 db_endpoint = os.environ.get("DB_ENDPOINT" , "lake-freeze-backend-db.cluster-cu0bcthnum69.us-east-1.rds.amazonaws.com")
 
+
+print("getting creds from sm")
 secret = json.loads(
         boto3.client("secretsmanager", 'us-east-1')
         .get_secret_value(SecretId=secret_arn)
@@ -17,6 +19,8 @@ db_username = secret["username"]
 
 db_password = secret["password"]
 
+
+print("creating engine")
 engine = sqlmodel.create_engine(f'postgresql+psycopg2://{db_username}:{db_password}@{db_endpoint}') #/lake_freeze
 
 
